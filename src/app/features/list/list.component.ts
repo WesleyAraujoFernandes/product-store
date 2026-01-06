fe2bd81b0cd7cd02f5538b42695447c4c85d07bc
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2026-01-06 14:26:51 -0300

    feat: adiciona página de edição

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -5,7 +5,7 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -16,9 +16,15 @@ import { RouterLink } from '@angular/router';
 export class ListComponent implements OnInit {
   products: Product[] = [];
   productsService = inject(ProductsService);
+  router = inject(Router);
+
   ngOnInit(): void {
     this.productsService.getAll().subscribe((products) => {
       this.products = products;
     });
+  }
+
+  onEdit() {
+    this.router.navigateByUrl('/edit-product');
   }
 }
