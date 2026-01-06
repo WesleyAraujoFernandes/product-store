224b600773c464c0b82147f2ecc15b3c20089605
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2026-01-06 09:22:58 -0300

    fix: corrige cor do header e button

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -5,13 +5,19 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
