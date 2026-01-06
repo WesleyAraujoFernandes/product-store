2cc34006900d429527bec7bf53b05e5f5716c74e
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2026-01-06 08:31:35 -0300

    feat: adiciona link para criar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -5,10 +5,11 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CardComponent],
+  imports: [MatCardModule, MatButtonModule, CardComponent, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
