95c2094908ceed9e40bbb41c97de2421cd380d81
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2026-01-08 10:28:23 -0300

    feat: criação do card com a messagem de lista de produtos vazia

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -10,6 +10,7 @@ import { CardComponent } from './components/card/card.component';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { filter, switchMap } from 'rxjs';
 import { ConfirmationDialogService } from './../../shared/services/confirmation-dialog.service';
+import { NoItemsComponent } from "./components/no-items/no-items.component";
 
 
 @Component({
@@ -21,12 +22,14 @@ import { ConfirmationDialogService } from './../../shared/services/confirmation-
     CardComponent,
     MatDialogModule,
     RouterLink,
+    NoItemsComponent
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
+  products = signal<Product[]>([]);
+  //products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
   productsService = inject(ProductsService);
   router = inject(Router);
   confirmationDialogService = inject(ConfirmationDialogService);
@@ -34,30 +37,7 @@ export class ListComponent {
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
   }
-  /*
-  onDelete(product: Product) {
-    this.confirmationDialogService.openDialog()
-      .pipe(filter(answer => answer === true))
-      .subscribe(() => {
-        this.productsService.delete(product.id).subscribe((answer) => {
-          this.productsService.getAll().subscribe((products) => {
-            this.products = this.products;
-          });
-        });
-      });
-  }
-  onDelete(product: Product) {
-    this.confirmationDialogService
-      .openDialog()
-      .pipe(filter(Boolean))
-      .subscribe(() => {
-        this.productsService.delete(product.id).subscribe(() => {
-          this.products = this.products.filter(p => p.id !== product.id);
-        });
-      });
-  }
 
-      */
   onDelete(product: Product) {
     this.confirmationDialogService
       .openDialog()
